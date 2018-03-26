5edc4ebfc0a7e6eef75b2e0815749ab9cd964066
xingdongyu@xiaomi.com <xingdongyu@xiaomi.com>
2018-03-26 13:52:49 +0800

    add react page

--- app.js
+++ app.js
@@ -19,7 +19,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+(function() {
+  // Step 1: 引入 webpack 的配置文件和 生成 webpack 的编译器
+  var webpack = require('webpack');
+  var webpackConfig = require('./webpack.config');
+  var compiler = webpack(webpackConfig);
+  // Step 2: 将编译器挂载给 webpack dev middleware
+  app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true, publicPath: webpackConfig.output.publicPath
+  }));
+
+  // Step 3: 将编译器挂载给 webpack hot middleware
+  app.use(require("webpack-hot-middleware")(compiler, {
+    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+  }));
+})();
+
+
+// app.use('/', indexRouter);
+app.use('/',function(req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack =  require('webpack')
+var HtmlwebpackPlugin = require('html-webpack-plugin')
+var path = require('path')
+module.exports = {
+  //入口
+  entry: './src/index.js',
+  //出口
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+    publicPath: './public'
+  },
+  //加载器
+  module: {
+    loaders: [
+        { test: /\.less$/, loader: 'style!css!less' },
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.(gif|jpg|png)$/, loader: 'url?limit=8192&name=images/[name].[hash].[ext]' },
+        { test: /\.(woff|svg|eot|ttf)$/, loader: 'url?limit=50000&name=fonts/[name].[hash].[ext]' }
+    ]
+  },  
+   //插件
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'nodepro',
+      filename: 'index.html'
+    }),
+  ]
+ 
+}
